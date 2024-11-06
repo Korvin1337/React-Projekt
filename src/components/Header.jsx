c0631296918e68d27b61ce60c16fc9233171e29d
Korvin1337 <korvin1337@gmail.com>
2024-11-06 15:13:35 +0100

    added hamburger menu javascript and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 const Header = ({ toggleDarkMode, isDarkMode }) => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setMenuOpen(!menuOpen)
+    }
+
+
     return (
         <header className={`${isDarkMode ? 'dark-mode' : ''}`}>
             <div className="container">
@@ -28,9 +36,16 @@ const Header = ({ toggleDarkMode, isDarkMode }) => {
                     </i>
                 </a>
 
-                <button className="btn-mobile">
-
+                <button className="btn-mobile" onClick={handleMenuToggle}>
                     <i className="fa-solid fa-bars"></i>
+
+                    {/* Show if menu opens */}
+                    {menuOpen && (
+                        <nav className="hamburger-menu">
+                            <Link to ="/" onClick={handleMenuToggle} className="hamburger-link hamburger-link-1">Features</Link>
+                            <Link to ="/contact" onClick={handleMenuToggle} className="hamburger-link hamburger-link-2">Contact</Link>
+                        </nav>
+                    )}
 
                 </button>
 
